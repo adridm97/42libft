9e9610a17b86c2159f3896722c817e4faf641276
adridm97 <adri.d.m1997@gmail.com>
2023-05-03 19:21:54 +0200

    creacion de los ficheros

---
+++ libft.h
@@ -0,0 +1,14 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+# include <stddef.h>
+
+
+//Delcaramos las funciones:
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+size_t  ft_strlen(const char *s);
+
+#endif
