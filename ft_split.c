e44c30d2ba6648ccf89896385c8b9ecd5a9e70ea
Adrian Duenas Moreira <aduenas-@cbr11s3.42barcelona.com>
2023-06-05 21:33:07 +0200

    de itoa a strmapi

--- ft_split.c
+++ ft_split.c
@@ -6,14 +6,67 @@
 /*   By: aduenas- <aduenas-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 22:21:40 by aduenas-          #+#    #+#             */
-/*   Updated: 2023/05/29 22:23:17 by aduenas-         ###   ########.fr       */
+/*   Updated: 2023/05/31 21:06:29 by aduenas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 static int ft_count_c(char const *s, char c)
+{
+	int	i;
+	int n;
 
+	i = 0;
+	n = 0;
+	while (s[i])
+	{
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == 0))
+			n++;
+		i++;
+	}
+	return (n);
+}
+char	*ft_word(const char *s, int start, int end)
+{
+	size_t	i;
+	char	*word;
+
+	i = 0;
+	word = malloc(sizeof(char) * (end - start + 1));
+	if (!word)
+		return (0);
+	while (start < end)
+	{
+		word[i] = s[start];
+		i++;
+		start++;
+	}
+	word[i] = 0;
+	return (word);
+}
+void	ft_write(char **split, char const *s, char c)
+{
+	size_t i;
+	size_t j;
+	int	copy;
+
+	i = 0;
+	j = 0;
+	copy = -1;
+	while ( i <= ft_strlen(s))
+	{
+		if (s[i] != c && copy < 0)
+			copy = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && copy >= 0)
+		{
+			split[j++] = ft_word(s, copy, i);
+			copy = -1;
+		}
+		i++;
+	}
+	split[j] = 0;
+}
 char	**ft_split(char const *s, char c)
 {
 	char **split;
